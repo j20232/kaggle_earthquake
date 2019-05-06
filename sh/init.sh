7deb7605366be70a81259c7a6c7bdaa063ee4178
mocobt <mocobt@gmail.com>
2019-05-06 14:50:36 +0900

    Revise path

--- sh/init.sh
+++ sh/init.sh
@@ -5,5 +5,5 @@ python src/101_quantile_aggregation.py 001
 python src/102_fft.py 001
 python src/103_signal_processing.py 001
 python src/104_window.py 001
-python 500/naive_kfolds 500_001
+python src/500_naive_kfolds.py 500_001
 python src/600_lightgbm.py 600_001
