517e0ba99442c6c130ccfc716bf4ca35c0f2de12
mocobt <mocobt@gmail.com>
2019-05-08 00:21:34 +0900

    Add kernel script

---
+++ sh/lan1.sh
@@ -0,0 +1,10 @@
+python src/000_to_feather.py
+python src/001_naive_split.py
+python src/100_simple_aggregation.py 001
+python src/101_quantile_aggregation.py 001
+python src/102_fft.py 001
+python src/103_signal_processing.py 001
+python src/104_window.py 001
+python src/501_spliting_on_earthquakes.py 501_001
+python src/600_train_with_lightgbm.py 600_002
+python src/800_predict_with_lightgbm.py 600_002
